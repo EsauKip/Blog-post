83cf42507dc5601bafa7154dbcade9d3fb9c0b84
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-12 23:18:03 +0300

    update imports,add main route to the index.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,16 @@
+from flask import render_template,request,redirect,url_for,abort
 from . import main
-from flask_login import login_required
-from flask import render_template
+from .forms import UpdateProfile
+from .. import db,photos
+from ..requests import get_quote
+from flask_login import current_user, login_required
+from ..models import User, Blog, Comment,Subscriber
+from app.main.forms import BlogForm,CommentForm
+from datetime import datetime
 
-@main.route('/')
-@login_required
+@main.route("/", methods=["GET", "BLOG"])
 def index():
+    blogs = Blog.get_all_blogs()
+    quote = get_quote()
 
-
-    return render_template('index.html')
+    return render_template("index.html", blogs=blogs, quote=quote)
