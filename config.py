8f1865f72437e01f6736b129c6ff7ad19dcc3b78
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-13 00:44:33 +0300

    add destination for photos,configure servers

--- config.py
+++ config.py
@@ -1,26 +1,34 @@
 import os
+import re
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kiprono12@localhost/blogpost'
+    '''
+    General configuration parent class
+    '''
+    
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:kiprono12@localhost/post'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
+   
 
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
     '''
-    Pruduction  configuration child class
+    Production  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
     pass
-
-class TestConfig(Config):
-
-
-    pass
-   
 
 class DevConfig(Config):
     '''
@@ -30,8 +38,8 @@ class DevConfig(Config):
     '''
    
     DEBUG = True
+
 config_options = {
 'development':DevConfig,
-'production':ProdConfig,
-
-}    
+'production':ProdConfig
+}
