7b1005045335748d88b3f60e03d6b1b415553f7d
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-13 22:21:19 +0300

    create new virtual,modify requirements

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    
+    SECRET_KEY='1a6c4143b228d08b6025c2e4b543c627'
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:kiprono12@localhost/blogpost'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='1a6c4143b228d08b6025c2e4b543c627'
+export MAIL_USERNAME='kipronoesau28@gmail.com'
+export MAIL_PASSWORD='#Youcannotfind5'
+
+python3 manage.py server
